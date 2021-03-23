2f7d59df20e20a7178af030c44cf5d5dd5aa656b
bhargavimohan <44068920+bhargavimohan@users.noreply.github.com>
2021-03-23 13:01:12 +0100

    Update config.py
    password change

--- src/config.py
+++ src/config.py
@@ -11,6 +11,6 @@ class DefaultConfig:
 
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "5a0f653d-152c-4a78-ab1a-d199ad22312e")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "kiX0v85-.z9x3O~lD5l9_SatgCez.r8HVi")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "125JkhPa.3SM4WXMJRxO.8K874jqN_~1.-")
 
     #bhargavi@mstbotdev.onmicrosoft.com
